2b0b80bdbe05f740486769f7de125f1fa7569889
alexandrexpj <alexandrexpjr@hotmail.com>
2021-10-07 19:05:29 -0300

    exercicios 1 e 2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,41 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPokemon: 0,
+            filteredType: 'Fire',
+        }
+    }
+
+    nextPoke = () => {
+        this.setState((estadoAnterior, props) => ({
+            currentPokemon: ((estadoAnterior.currentPokemon + 1) < (this.filterPoke().length))? estadoAnterior.currentPokemon + 1 : 0,
+        }))
+    }
+
+    setType = (type) => {
+        this.setState({
+            filteredType: type
+        })
+        console.log(this.state)
+    }
+
+    filterPoke = () => {
+        const { pokemons } = this.props;
+        return pokemons.filter(pokemon => pokemon.type === this.state.filteredType)
+    }
+
+
     render() {
+        const filteredPokemons = this.filterPoke();
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={filteredPokemons[this.state.currentPokemon].id} pokemon = {filteredPokemons[this.state.currentPokemon]}/>
+                <button onClick={this.nextPoke}>Próximo</button>
+                <button onClick={() => this.setType('Psychic')}>Psychic</button>
+                <button onClick={() => this.setType('Fire')}>Fire</button>
             </div>
         );
     }
